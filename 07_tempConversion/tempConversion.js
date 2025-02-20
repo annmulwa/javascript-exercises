8d09c0f483492c61b199b3f04aad572884cd3e3a
annmulwa <annmulwa2000@gmail.com>
2025-02-20 15:06:50 +0300

    CONVERT TEMP Converting temperature to celsius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fanhrenheit) {
+  const celsiusTemp = (fanhrenheit - 32) * 5/9;
+  return (parseFloat(celsiusTemp.toFixed(1)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fanhrenheitTemp = (celsius * 9/5) + 32;
+  return (parseFloat(fanhrenheitTemp.toFixed(1)));
 };
 
 // Do not edit below this line
