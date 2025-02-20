c9523be0054403242f6a462d7d40c73420e002b1
annmulwa <annmulwa2000@gmail.com>
2025-02-20 13:17:51 +0300

    SUM ALL NUMBERS Summing all numbers in a given range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(initialNum, finalNum) {
+    let sum = 0;
+    let i;
+    if (!Number.isInteger(initialNum) || !Number.isInteger(finalNum)) {
+        return ("ERROR");
+    } else if (initialNum < 0 || finalNum < 0) {
+        return ("ERROR");
+    } else if (initialNum < finalNum) {
+        for (i = initialNum; i <= finalNum; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (initialNum > finalNum) {
+        for (i = finalNum; i <= initialNum; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
